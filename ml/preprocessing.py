9d1266c405bf144c535c159557610c462aaa2110
nnbIT <211180240+nnbIT@users.noreply.github.com>
2025-11-22 23:28:52 -0500

    Update preprocessing.py

--- ml/preprocessing.py
+++ ml/preprocessing.py
@@ -1,6 +1,40 @@
+import pandas as pd
+from pathlib import Path
 from ml.feature_extraction import extract_features
 
-url = "https://paypal-login-secure-confirm-update.com/login?session=123&auth=true"
+# Paths
+BASE_DIR = Path(__file__).resolve().parent.parent
+RAW_DATA = BASE_DIR / "data" / "raw" / "processed"/ "data.xlsx"
+PROCESSED_DATA = BASE_DIR / "data" / "raw" / "processed" / "data_processed.csv"
 
-features = extract_features(url)
-print(features)
+def process_dataset():
+    print(f"Loading dataset from: {RAW_DATA}")
+
+    # Load raw csv (must contain: url,label)
+    df = pd.read_excel(RAW_DATA)
+
+    if "url" not in df.columns or "label" not in df.columns:
+        raise ValueError("❌ data.csv must contain 'url' and 'label' columns")
+
+    print("Extracting features…")
+
+    processed_rows = []
+
+    for _, row in df.iterrows():
+        url = row["url"]
+        label = row["label"]
+
+        features = extract_features(url)
+        features["label"] = label  # add label at the end
+
+        processed_rows.append(features)
+
+    # Convert to DataFrame
+    processed_df = pd.DataFrame(processed_rows)
+
+    # Save
+    processed_df.to_csv(PROCESSED_DATA, index=False)
+    print(f"✅ Processing complete! Saved to:\n{PROCESSED_DATA}")
+
+if __name__ == "__main__":
+    process_dataset()
