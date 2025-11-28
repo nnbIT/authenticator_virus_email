4454f74d94e892dfb8523432e60e5e7306244603
nnbIT <211180240+nnbIT@users.noreply.github.com>
2025-11-28 00:37:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import url_scanner, file_scanner, email_scanner
 
 app = FastAPI(
     title="Cybersecurity Analyzer API",
     description="API to scan URLs, files, and emails to detect malware.",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Your React frontend
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Register routers
